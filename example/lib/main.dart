7b9c9743fe073fde3c8bbf017b0f72f8cfb3d607
mcmah309 <dillonmcmahon79@gmail.com>
2024-09-28 17:23:29 +0000

    test: Add color example test

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,36 @@ import 'package:wind_ui/wind_ui.dart';
 
 final appTheme = AppTheme();
 
-class AppTheme extends FullTheme {}
+class AppTheme extends FullTheme {
+  @override
+  Color2 get textSelection => Color2.material(Colors.blue);
+  @override
+  Color2 get textSelectionCursor => Color2.material(Colors.blueGrey);
+
+  // @override
+  // Color2 get action => throw UnimplementedError();
+
+  // @override
+  // Color2 get destructive => throw UnimplementedError();
+
+  // @override
+  // Color2 get focalPoint => throw UnimplementedError();
+
+  // @override
+  // Color2 get positive => throw UnimplementedError();
+
+  @override
+  Color2 get primary => Color2.material(Colors.amber);
+
+  // @override
+  // Color2 get secondary => throw UnimplementedError();
+
+  // @override
+  // Color2 get surface => throw UnimplementedError();
+
+  // @override
+  // Color2 get warning => throw UnimplementedError();
+}
 
 void main() {
   runApp(const MyApp());
@@ -82,6 +111,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   case 1:
                     body = const TextTest();
                     _currentIndex = 1;
+                  case 2:
+                    body = const Color2Test();
+                    _currentIndex = 2;
                   default:
                     body = null;
                     _currentIndex = 0;
@@ -89,7 +121,8 @@ class _MyHomePageState extends State<MyHomePage> {
               }),
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.text_format), label: "Text Test")
+            BottomNavigationBarItem(icon: Icon(Icons.text_format), label: "Text Test"),
+            BottomNavigationBarItem(icon: Icon(Icons.colorize), label: "Color2 Test")
           ]),
     );
   }
@@ -105,43 +138,51 @@ class TextTest extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Flexible(
-            child: Column(
-              children: [
-                const Text("Normal"),
-                linesNormal(appTheme.textXs),
-                linesNormal(appTheme.textSm),
-                linesNormal(appTheme.textBase),
-                linesNormal(appTheme.textLg),
-                linesNormal(appTheme.textXl),
-                linesNormal(appTheme.text2Xl),
-                linesNormal(appTheme.text3Xl),
-                linesNormal(appTheme.text4Xl),
-                linesNormal(appTheme.text5Xl),
-                linesNormal(appTheme.text6Xl),
-                linesNormal(appTheme.text7Xl),
-                linesNormal(appTheme.text8Xl),
-                linesNormal(appTheme.text9Xl),
-              ],
+            child: SelectableRegion(
+              focusNode: FocusNode(),
+              selectionControls: MaterialTextSelectionControls(),
+              child: Column(
+                children: [
+                  const Text("Normal"),
+                  linesNormal(appTheme.textXs),
+                  linesNormal(appTheme.textSm),
+                  linesNormal(appTheme.textBase),
+                  linesNormal(appTheme.textLg),
+                  linesNormal(appTheme.textXl),
+                  linesNormal(appTheme.text2Xl),
+                  linesNormal(appTheme.text3Xl),
+                  linesNormal(appTheme.text4Xl),
+                  linesNormal(appTheme.text5Xl),
+                  linesNormal(appTheme.text6Xl),
+                  linesNormal(appTheme.text7Xl),
+                  linesNormal(appTheme.text8Xl),
+                  linesNormal(appTheme.text9Xl),
+                ],
+              ),
             ),
           ),
           Flexible(
-            child: Column(
-              children: [
-                const Text("Wind Ui"),
-                lines(appTheme.textXs),
-                lines(appTheme.textSm),
-                lines(appTheme.textBase),
-                lines(appTheme.textLg),
-                lines(appTheme.textXl),
-                lines(appTheme.text2Xl),
-                lines(appTheme.text3Xl),
-                lines(appTheme.text4Xl),
-                lines(appTheme.text5Xl),
-                lines(appTheme.text6Xl),
-                lines(appTheme.text7Xl),
-                lines(appTheme.text8Xl),
-                lines(appTheme.text9Xl),
-              ],
+            child: SelectableRegion(
+              focusNode: FocusNode(),
+              selectionControls: MaterialTextSelectionControls(),
+              child: Column(
+                children: [
+                  const Text("Wind Ui"),
+                  lines(appTheme.textXs),
+                  lines(appTheme.textSm),
+                  lines(appTheme.textBase),
+                  lines(appTheme.textLg),
+                  lines(appTheme.textXl),
+                  lines(appTheme.text2Xl),
+                  lines(appTheme.text3Xl),
+                  lines(appTheme.text4Xl),
+                  lines(appTheme.text5Xl),
+                  lines(appTheme.text6Xl),
+                  lines(appTheme.text7Xl),
+                  lines(appTheme.text8Xl),
+                  lines(appTheme.text9Xl),
+                ],
+              ),
             ),
           ),
         ],
@@ -161,6 +202,167 @@ class TextTest extends StatelessWidget {
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer metus nibh, euismod non tempor quis, vulputate eu metus. Phasellus vitae dolor dignissim, pharetra eros at, varius magna. Sed eu hendrerit ipsum. Pellentesque sed fermentum diam, nec ullamcorper sapien. Pellentesque et sapien egestas, scelerisque leo id, tincidunt justo. Nunc in interdum lacus. Nullam in neque eget urna finibus consequat nec et erat.\nDonec sit amet scelerisque est. Suspendisse tempus interdum tincidunt. Praesent sit amet nulla eget felis accumsan lacinia. Morbi iaculis justo ipsum, in fringilla est pharetra sed. Donec ut efficitur risus. Donec fringilla, leo nec ultrices viverra, mi lectus feugiat ex, a ultrices nunc tortor in elit. Quisque elementum odio at nulla molestie semper. Maecenas orci nisl, convallis quis pellentesque sed, lobortis id massa. Proin dignissim arcu enim, non gravida lectus hendrerit a.",
           style: font,
         ),
+      ],
+    );
+  }
+}
+
+class Color2Test extends StatelessWidget {
+  const Color2Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            test(Colors2.greyNeutral, "Grey Neutral"),
+            test(Colors2.greyCool, "Grey Cool"),
+            test(Colors2.greyWarm, "Grey Warm"),
+            test(Colors2.greyScaleDark, "Grey Scale Dark"),
+            test(Colors2.greyScaleLight, "Grey Scale Light"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget test(Color2 color, String label) {
+    return Column(
+      children: [
+        Text(label),
+        Row(
+          children: [
+            Column(
+              children: [
+                const Text("50"),
+                Container(
+                  width: 30,
+                  height: 30,
+                  color: color.shade50,
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Column(
+              children: [
+                const Text("100"),
+                Container(
+                  width: 30,
+                  height: 30,
+                  color: color.shade100,
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Column(
+              children: [
+                const Text("200"),
+                Container(
+                  width: 30,
+                  height: 30,
+                  color: color.shade200,
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Column(
+              children: [
+                const Text("300"),
+                Container(
+                  width: 30,
+                  height: 30,
+                  color: color.shade300,
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Column(
+              children: [
+                const Text("400"),
+                Container(
+                  width: 30,
+                  height: 30,
+                  color: color.shade400,
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Column(
+              children: [
+                const Text("500"),
+                Container(
+                  width: 30,
+                  height: 30,
+                  color: color.shade500,
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Column(
+              children: [
+                const Text("600"),
+                Container(
+                  width: 30,
+                  height: 30,
+                  color: color.shade600,
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Column(
+              children: [
+                const Text("700"),
+                Container(
+                  width: 30,
+                  height: 30,
+                  color: color.shade700,
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Column(
+              children: [
+                const Text("800"),
+                Container(
+                  width: 30,
+                  height: 30,
+                  color: color.shade800,
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Column(
+              children: [
+                const Text("900"),
+                Container(
+                  width: 30,
+                  height: 30,
+                  color: color.shade900,
+                ),
+              ],
+            ),
+          ],
+        )
       ],
     );
   }
